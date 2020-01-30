4630ea0ef0eb08f42e1616cfebd2fff37c3947ee
siggioh <42964024+siggioh@users.noreply.github.com>
2020-01-30 16:49:30 +0000

    Update fair_traffic_lights.py

--- fair_traffic_lights.py
+++ fair_traffic_lights.py
@@ -1,11 +1,59 @@
-s_to_n = int(input('Number of cars travelling north:'))
-n_to_s = int(input('Number of cars travelling south:'))
-e_to_w = int(input('Number of cars travelling west:'))
-w_to_e = int(input('Number of cars travelling east:'))
 
-# Traffík samtals í bæði Norður og Suður
-n_and_s_total = s_to_n + n_to_s
-w_and_W_total = e_to_w + w_to_e
+north_int = int(input("Number of cars travelling north: "))
+south_int = int(input("Number of cars travelling south: "))
+east_int = int(input("Number of cars travelling east: "))
+west_int = int(input("Number of cars travelling west: "))
+
+
+while True:
+    cars_total = 0
+    
+    cars_total = north_int + south_int + west_int + east_int
+    n_and_s_total = north_int + south_int
+    w_and_e_total = west_int + east_int
+    
+    if n_and_s_total > 0 or w_and_e_total > 0:
+        if n_and_s_total >= w_and_e_total:
+            print('Green light on N/S')
+            north_int -= 5
+            
+            if north_int < 0:
+                
+                north_int = 0
+            
+            south_int -= 5
+            
+            if south_int < 0:
+                south_int = 0
+            
+            
+
+        else:
+            print('Green light on E/W')
+            west_int -= 5
+            if west_int < 0:
+                west_int = 0
+            
+            east_int -= 5
+            if east_int < 0:
+                east_int = 0
+        
+        
+    else: 
+        if cars_total == 0:
+            print('No cars waiting, the traffic jam has been solved!')
+            break
+    
+
+
+
+    
+
+
+
+
+
+
 
 
 
