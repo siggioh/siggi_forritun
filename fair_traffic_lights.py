97c1a4d2b46249adbe67f2439475d97b8bf17f1f
siggioh <42964024+siggioh@users.noreply.github.com>
2020-01-30 15:58:00 +0000

    Update fair_traffic_lights.py

---
+++ fair_traffic_lights.py
@@ -0,0 +1,12 @@
+s_to_n = int(input('Number of cars travelling north:'))
+n_to_s = int(input('Number of cars travelling south:'))
+e_to_w = int(input('Number of cars travelling west:'))
+w_to_e = int(input('Number of cars travelling east:'))
+
+# Traffík samtals í bæði Norður og Suður
+n_and_s_total = s_to_n + n_to_s
+w_and_W_total = e_to_w + w_to_e
+
+
+
+
